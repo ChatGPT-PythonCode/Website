4ae5e00566ef1df7a4d3453e5522575785934cd0
Luls <152764488+ChatGPT-PythonCode@users.noreply.github.com>
2026-04-13 14:55:22 -0700

    Update blog-config.js

--- js/blog-config.js
+++ js/blog-config.js
@@ -1,8 +1,8 @@
 window.LULS_CONFIG = Object.assign(
   {
     // Change these two lines if your active Blogger address is different.
-    bloggerSiteUrl: "https://eoguides.com/",
-    bloggerFeedBase: "https://eoguides.com/feeds/posts/default",
+    bloggerSiteUrl: "http://eoguides.com/",
+    bloggerFeedBase: "http://eoguides.com/feeds/posts/default",
 
     siteName: "EO Guides",
     tcgUrl: "https://tcg.luls.lol/",
